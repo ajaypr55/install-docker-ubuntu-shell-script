d748307a94a2bbdc69e5833ff91aaad5e2181990
Ajay Prakash <ajaypr55@gmail.com>
2019-09-04 08:33:16 +0530

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -17,3 +17,7 @@ sudo apt-get -y install docker-engine docker-compose
 
 # add current user to docker group so there is no need to use sudo when running docker
 sudo usermod -aG docker $(whoami)
+
+
+docker build -t tfm:tfm2 .
+Run docker : docker run -it --rm -v /datadrive/notebooks:/tf/notebooks -p 8888:8888 -p 6006:6006  tfm:tfm2
